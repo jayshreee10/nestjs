10ec0337ce772c8d50fb37ea4f433f958a67fd70
Jayashree Sarangi (NTE-907) <2001109280_ee@pmec.ac.in>
2025-06-02 15:48:29 +0530

    userResource

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,46 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
   @Get()
-  getUsers() {
+  findAll() {
     return this.usersService.findAll();
   }
 
-  @Post()
-  createUser(@Body('name') name: string) {
-    return this.usersService.create(name);
-    // return console.log(name)
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.remove(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,83 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 @Injectable()
 export class UsersService {
-  private users = [{id:1,name:"hii"}];
+  public users: User[] = [
+  {
+    id: 1,
+    name: 'Alice Johnson',
+    email: 'alice@example.com',
+    age: 28,
+  },
+  {
+    id: 2,
+    name: 'Bob Smith',
+    email: 'bob@example.com',
+    age: 34,
+  },
+  {
+    id: 3,
+    name: 'Charlie Brown',
+    email: 'charlie@example.com',
+    age: 22,
+  },
+  {
+    id: 4,
+    name: 'Diana Prince',
+    email: 'diana@example.com',
+    age: 30,
+  },
+  {
+    id: 5,
+    name: 'Ethan Hunt',
+    email: 'ethan@example.com',
+    age: 40,
+  },
+];
+  private idCount=1;
 
-  findAll() {
+  create(createUserDto: CreateUserDto) : User[] {
+    const newUser : User = {
+      id: this.idCount++,
+      ...createUserDto
+    };
+    this.users.push(newUser);
+     return this.users;
+  }
+
+  findAll():User[] {
     return this.users;
   }
 
-  create(name: string) {
-    this.users.push({ id: this.users.length + 1, name });
-    return { message: 'User created',newArr: this.users } ;
+  findOne(id: number): User {
+     const user = this.users.find((user)=>user.id === id);
+     if(!user){
+      throw new NotFoundException(`User with ID ${id} not found`);
+     }
+     return user;
   }
+
+
+   update(id: number, updateUserDto: UpdateUserDto): User{
+    const user = this.findOne(id);
+    console.log(user);
+    const updatedUser = { ...user, ...updateUserDto };
+    console.log(updatedUser)
+
+    this.users = this.users.map((u) => (u.id === id ? updatedUser : u));
+    return user;
+  }
+
+  remove(id: number) : User[] {
+    const index = this.users.findIndex((user)=>user.id===id);
+    if(index === -1){
+    throw new NotFoundException(`User with ID ${id} not found`);
+    }
+      this.users.splice(index, 1);
+      return this.users
+  }
+  
+  
 }
